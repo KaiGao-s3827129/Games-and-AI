39887e48a26394425fff78d896bfeaf6e5f6d5a3
Jeffrey Sun <s3766167@student.rmit.edu.au>
2022-03-31 00:54:06 +0800

    Update player movement

--- Assets/Scripts/NeoMovement.cs
+++ Assets/Scripts/NeoMovement.cs
@@ -6,28 +6,78 @@ using UnityEngine;
 public class NeoMovement : MonoBehaviour
 {
     private Rigidbody2D neo;
+    private Animator anim;
 
-    public float speed;
+    public LayerMask mask;
+    public float boxHeight = 0.05f;
+    public float jumpValue = 60f;
+    public float speed = 20f;
+    public float fallMulti = 20f;
+    public float lowJumpMulti = 25f;
+    public int jumpCount;
+    public int healthPoint = 3;
+    public bool isGetWeapon = false;
+    public bool isJump = false;
 
+    private Vector2 playerSize;
+    private Vector2 boxSize;
     private float horizontalMove;
-    private bool isJumpPress;
+    private bool jumpRequest = false;
+    private bool isGround = false;
     
     // Start is called before the first frame update
     void Start()
     {
         neo = GetComponent<Rigidbody2D>();
+        playerSize = GetComponent<SpriteRenderer>().bounds.size;
+        boxSize = new Vector2(playerSize.x * 0.0f, boxHeight);
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        if (Input.GetButtonDown("Jump") && isGround)
+        {
+            jumpRequest = true;
+        }
+        SwitchAnim();
     }
 
-    // void FixedUpdate()
-    // {
-    //     Run();
-    // }
+    private void FixedUpdate()
+    {
+        if (jumpRequest)
+        {
+            neo.AddForce(Vector2.up * jumpValue, ForceMode2D.Impulse);
+            jumpRequest = false;
+        }
+        else
+        {
+            Vector2 boxCenter = (Vector2) transform.position + (Vector2.down * playerSize.y * 0.5f);
+
+            if (Physics2D.OverlapBox(boxCenter, boxSize, 0, mask) != null)
+            {
+                isGround = true;
+            }
+            else
+            {
+                isGround = false;
+            }
+        }
+        
+        if (neo.velocity.y < 0)
+        {
+            neo.gravityScale = fallMulti;
+        } else if (neo.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            neo.gravityScale = lowJumpMulti;
+        }
+        else
+        {
+            neo.gravityScale = 10f;
+        }
+    }
 
     private void Run()
     {
@@ -39,5 +89,55 @@ public class NeoMovement : MonoBehaviour
             transform.localScale = new Vector3(horizontalMove, 1, 1);
         }
     }
-    
+
+    void SwitchAnim()
+    {
+        anim.SetFloat("running", Mathf.Abs(neo.velocity.x));
+
+        if (isGround)
+        {
+            anim.SetBool("falling", false);
+            anim.SetBool("jumping", false);
+        }
+        else if (!isGround && neo.velocity.y > 0)
+        {
+            anim.SetBool("jumping", true);
+            anim.SetBool("falling", false);
+        }
+        else if (neo.velocity.y < 0)
+        {
+            anim.SetBool("jumping", false);
+            anim.SetBool("falling", true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "WeaponBox" && !isGetWeapon)
+        {
+            isGetWeapon = true;
+            col.GetComponent<Animator>().SetTrigger("get");
+            Destroy(col.gameObject, 1.5f);
+        }
+        
+        if (col.tag == "SkillBox" && jumpCount == 1)
+        {
+            jumpCount += 1;
+            col.GetComponent<Animator>().SetTrigger("get");
+            Destroy(col.gameObject, 1.5f);
+        }
+
+        if (col.tag == "Enemy")
+        {
+            healthPoint -= 1;
+            if (healthPoint == 0)
+            {
+                // die
+            }
+            else
+            {
+                // invincibility
+            }
+        }
+    }
 }
